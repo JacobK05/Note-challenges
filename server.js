f45bf614e4eb9e19693c3ab9eef14e8f36bfa248
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-01-28 14:19:37 -0600

    added app.get and post req"

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
 // importing express and add built in node path
 const express = require("express")
 const path = require('path')
-// intisallizing express 
-const app = express;
+const fs = require('fs')
+// intisallizing express
+const uuid = require('./helpers/uuid');
+const app = express();
 const PORT = 3001;
 // middle ware pointing to the public folder 
 app.use(express.static('public'));
-// get calls 
-app.get('/', (req, res) => res.send)('default api')
+app.use(express.urlencoded({extended: true }));
+app.use(express.json())
 
-app.get('/notes', (req , res) =>
-res.send(path.join(__dirname, 'public/notes.html'))
+module.exports = (app) =>{
+// get calls
+app.get('*', (req , res) =>
+res.send(path.join(__dirname, '../public/notes.html'))
 )
 
+app.get('/notes', (req , res) =>
+res.sendFile(path.join(__dirname, '../public/notes.html'))
+)
+
+app.get('/api/notes', (req, res) => {
+ res.sendFile(path.join(__dirname, '../db/db.json'))
+})
+
+app.post('api/notes', (req, res) => {
+
+const { title, text } = req.body
+
+if (title && text){
+ const newNote = {
+    title,
+    text,
+    review_id: uuid()
+ }
+}
+
+})
+
+
+};
 
 
 
