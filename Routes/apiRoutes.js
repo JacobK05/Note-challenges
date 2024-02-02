a6e6619a4f36b52126c4fc9c9da9318380945cb4
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-02-02 12:30:21 -0600

    fixed html route

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,32 +1,29 @@
 const path = require('path')
 const fs = require('fs')
-
+const router = require('express').Router()
 const uniqid = require('uniqid'); 
 
 
 
-module.exports = (app) => {
 
 
-app.get('/api/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
  res.sendFile(path.join(__dirname, '../db/db.json'))
 })
 
-app.post('/api/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
 
-let db = fs.readFileSync(`db/db.json`);
- db = JSON.parse(db);
- res.json(db);
     
-let newNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: uniqid()
-    };
+   const stringNote = JSON.stringify(newNote);
 
- db.push(newNote);
-    fs.writeFileSync(`db/db.json`, JSON.stringify(db));
-    res.json(db);
-
-})
-}
+   fs.writeFile('./db/db.json', stringNote, (err) => {
+     if (err) {
+       console.error(err);
+       res.status(500).json({ error: 'Failed to save note' });
+     } else {
+       res.json(newNote);
+     }
+   });
+ })
+ 
+module.exports = router
--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -1,17 +1,17 @@
 const path = require('path')
+const router = require('express').Router()
 
 
 
 
-module.exports = (app) =>{
 
-app.get('/notes', (req , res) =>
- res.send(path.join(__dirname, '../public/notes.html'))
+router.get('/notes', (req , res) =>
+ res.sendFile(path.join(__dirname, '../public/notes.html'))
 )
 
 
-app.get('*', (req , res) =>
- res.send(path.join(__dirname, '../public/index.html'))
+router.get('*', (req , res) =>
+ res.sendFile(path.join(__dirname, '../public/index.html'))
 )
 
-}
+module.exports = router
