2eac42e0583ae72acd2a83446ae20b42404eae02
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-02-01 22:14:29 -0600

    added files fix

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require("express")
-
+const apiRoutes = require("./Routes/apiRoutes")
+const htmlRoutes = require("./Routes/htmlRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use(express.static('public'));
 
+//app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+//require('./Routes/apiRoutes')(app)
 
-require('./Routes/apiRoutes')(app)
-
-require('./Routes/htmlRoute')(app)
+//require('./Routes/htmlRoute')(app)
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
